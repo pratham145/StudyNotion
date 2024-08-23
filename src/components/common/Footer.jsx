3eee99e774d1ba6c06bf8d6f4d1c5d35b8f2b16c
pratham rameshwar <121504583+pratham145@users.noreply.github.com>
2024-08-23 12:15:49 +0530

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FooterLink2 } from "../../data/footer-links";
 import { Link } from "react-router-dom";
 
-// Images
+// Imagess 
 import Logo from "../../assets/Logo/Logo-Full-Light.png";
 
 // Icons
